c7f4f1e1cbdb9fe83b0ba7300182e257a4424344
@MShakeel <mshakeel@genese.de>
2023-03-08 00:12:18 +0100

    Wraps CharitySideBarContent's buttons in react router Link components

--- src/components/CharityDashboard/CharitySideBarContent/CharitySideBarContent.tsx
+++ src/components/CharityDashboard/CharitySideBarContent/CharitySideBarContent.tsx
@@ -8,6 +8,7 @@ import {
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {};
 
@@ -15,19 +16,23 @@ const CharitySideBarContent = (props: Props) => {
     return (
         <div className="flex w-full flex-col overflow-y-auto pt-6">
             <List component="nav">
-                <ListItemButton>
-                    <ListItemIcon>
-                        <ViewListIcon fontSize="large" />
-                    </ListItemIcon>
-                    <ListItemText primary="Manage active events" />
-                </ListItemButton>
+                <Link to="/charity-dashboard/manage">
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <ViewListIcon fontSize="large" />
+                        </ListItemIcon>
+                        <ListItemText primary="Manage active events" />
+                    </ListItemButton>
+                </Link>
 
-                <ListItemButton>
-                    <ListItemIcon>
-                        <AddBoxIcon fontSize="large" />
-                    </ListItemIcon>
-                    <ListItemText primary="Create new events" />
-                </ListItemButton>
+                <Link to="/charity-dashboard/create">
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <AddBoxIcon fontSize="large" />
+                        </ListItemIcon>
+                        <ListItemText primary="Create new events" />
+                    </ListItemButton>
+                </Link>
             </List>
             {/* <div className="mb-3 flex items-center justify-between pt-4 opacity-60">
                 <span className=""></span>
