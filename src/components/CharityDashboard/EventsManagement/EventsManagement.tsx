4fd811b6f468ddc308899a683de22a13866c10ee
@MShakeel <mshakeel@genese.de>
2023-03-08 23:07:06 +0100

    Adds EventsManagementListItem to EventsManagement.tsx

--- src/components/CharityDashboard/EventsManagement/EventsManagement.tsx
+++ src/components/CharityDashboard/EventsManagement/EventsManagement.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import { useTheme } from '@mui/material';
+import EventsManagementListItem from './EventsManagementListItem/EventsManagementListItem';
 
 type Props = {};
 
 const EventsManagement = (props: Props) => {
+    const theme = useTheme();
+
+    const testArrForList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const eventsList: JSX.Element[] = testArrForList.map((num) => (
+        <EventsManagementListItem key={num} />
+    ));
+
     return (
-        <div>
-            <h1>EventsManagement</h1>
+        <div
+            className="h-full w-full overflow-y-auto px-4 py-6"
+            style={{ backgroundColor: theme.palette.background.default }}
+        >
+            <h1 className="mb-16 text-2xl font-bold">EventsManagement</h1>
+
+            <div className="flex flex-col items-center gap-y-8">
+                {eventsList}
+            </div>
         </div>
     );
 };
---
+++ src/components/CharityDashboard/EventsManagement/EventsManagementListItem/EventsManagementListItem.tsx
@@ -0,0 +1,78 @@
+import { useTheme, Button, Divider } from '@mui/material';
+import React from 'react';
+import noImgPlaceholder from '../../../../assets/No-Image-Placeholder.png';
+import EditIcon from '@mui/icons-material/Edit';
+import MapIcon from '@mui/icons-material/Map';
+
+type Props = {};
+
+const EventsManagementListItem = (props: Props) => {
+    const theme = useTheme();
+
+    return (
+        <div
+            className="flex h-44 w-10/12 rounded-xl border-2"
+            style={{ borderColor: theme.palette.divider }}
+        >
+            <img src={noImgPlaceholder} alt="" className="w-44 rounded-l-xl" />
+            <div className="flex flex-1 flex-col gap-y-4 overflow-auto p-4">
+                <div>
+                    <span className="mr-1 text-lg font-semibold">
+                        Event title:
+                    </span>
+                    <span>Lorem ipsum dolor sit.</span>
+                </div>
+                <div>
+                    <span className="mr-1 text-lg font-semibold">
+                        Created on:
+                    </span>
+                    <span>08 January 2023, 13:14</span>
+                </div>
+                <div>
+                    <span className="mr-1 text-lg font-semibold">Ends on:</span>
+                    <span>Not defined.</span>
+                </div>
+                <div>
+                    <span className="mr-1 text-lg font-semibold">Address:</span>
+                    <span>
+                        Lorem ipsum dolor sit amet, 24581, Hamburg, Germany.
+                    </span>
+                </div>
+                <div>
+                    <span className="mr-1 text-lg font-semibold">
+                        Volunteers needed:
+                    </span>
+                    <span>None.</span>
+                </div>
+                <div>
+                    <span className="mr-1 text-lg font-semibold">
+                        Description:
+                    </span>
+                    <span>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Porro accusantium ut quibusdam suscipit totam,
+                        dignissimos animi corporis quos dolorum, veniam
+                        accusamus iste molestias officiis ex. Culpa facere
+                        ratione aperiam. Itaque, reiciendis voluptatibus quaerat
+                        consectetur harum ratione, placeat saepe in voluptates
+                        laborum, ea similique. Nobis sit laboriosam voluptatum
+                        dignissimos. Nemo, suscipit.
+                    </span>
+                </div>
+            </div>
+
+            <Divider flexItem orientation="vertical"></Divider>
+
+            <div className="flex flex-col justify-center gap-y-4 p-4">
+                <Button variant="outlined" endIcon={<EditIcon />}>
+                    Edit
+                </Button>
+                <Button variant="outlined" endIcon={<MapIcon />}>
+                    View on map
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default EventsManagementListItem;
