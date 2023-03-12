93df405408a4c1a6b14fd7a4df8b1cb1f2d1c96f
@MShakeel <mshakeel@genese.de>
2023-03-12 17:54:29 +0100

    Adds styling and changes background color for EventsCreation.tsx

--- src/components/CharityDashboard/EventsCreation/EventsCreation.tsx
+++ src/components/CharityDashboard/EventsCreation/EventsCreation.tsx
@@ -1,10 +1,16 @@
+import { useTheme } from '@mui/system';
 import React from 'react';
 
 type Props = {};
 
 const EventsCreation = (props: Props) => {
+    const theme = useTheme();
+
     return (
-        <div>
+        <div
+            className="h-full w-full overflow-auto p-4"
+            style={{ backgroundColor: theme.palette.background.default }}
+        >
             <h1>EventsCreation</h1>
         </div>
     );
