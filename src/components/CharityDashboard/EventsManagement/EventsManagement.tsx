8e78b78556eec9a16b9c14a70fd739cf1cd55fbb
@MShakeel <mshakeel@genese.de>
2023-03-25 22:19:54 +0100

    Adds AddOrEditDialog opening button to EventsManagement component

--- src/components/CharityDashboard/EventsManagement/EventsManagement.tsx
+++ src/components/CharityDashboard/EventsManagement/EventsManagement.tsx
@@ -1,12 +1,25 @@
+import { Button, useTheme } from '@mui/material';
 import AnimatedMain from '../../FramerMotion/AnimatedMain';
 import EventsManagementListItem from './EventsManagementListItem/EventsManagementListItem';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import AddOrEditEventDialog from './AddOrEditEventDialog/AddOrEditEventDialog';
+import { useState } from 'react';
 
 type Props = {};
 
 const EventsManagement = (props: Props) => {
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpenDialog(true);
+    };
+    const handleClose = () => {
+        setOpenDialog(false);
+    };
+
     const theme = useTheme();
 
-    const testArrForList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const testArrForList = [0, 1, 2, 3, 4];
 
     const eventsList: JSX.Element[] = testArrForList.map((num) => (
         <EventsManagementListItem key={num} />
@@ -14,7 +27,33 @@ const EventsManagement = (props: Props) => {
 
     return (
         <AnimatedMain>
+            <div
+                className="h-full w-full overflow-y-auto overflow-x-hidden px-12 py-6"
+                style={{ backgroundColor: theme.palette.background.default }}
+            >
+                <div className="mb-16 flex items-center justify-between">
+                    <h1 className="text-2xl font-bold">EventsManagement</h1>
 
+                    <Button
+                        variant="outlined"
+                        size="large"
+                        endIcon={<AddBoxIcon fontSize="large" />}
+                        onClick={handleClickOpen}
+                    >
+                        <span className="font-semibold">Add event</span>
+                    </Button>
+
+                    <AddOrEditEventDialog
+                        handleClose={handleClose}
+                        openDialog={openDialog}
+                        addOrEditString="Add"
+                    />
+                </div>
+
+                <div className="flex flex-col items-center gap-y-12">
+                    {eventsList}
+                </div>
+            </div>
         </AnimatedMain>
     );
 };
