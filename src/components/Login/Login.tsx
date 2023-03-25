e64b2845ebcbe717203ded7a2b99a7ddb7f78c96
@MShakeel <mshakeel@genese.de>
2023-03-25 22:25:19 +0100

    Changed styling in Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -12,6 +12,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import React from 'react';
 import Header from '../VolunteerDashboard/Header/Header';
+import AnimatedMain from '../FramerMotion/AnimatedMain';
 
 type Props = {
     mode: 'light' | 'dark';
@@ -23,32 +24,80 @@ function Login(props: Props) {
         <>
             <Header mode={props.mode} setMode={props.setMode} />
             <AnimatedMain>
+                <Container component="main" maxWidth="xs">
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Log in to Charity Meet
+                        </Typography>
+                        <Box component="form" noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            {/* <FormControlLabel
                             control={
                                 <Checkbox value="remember" color="primary" />
                             }
                             label="Remember me"
                         /> */}
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="secondary"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Log In
-                        </Button>
-                        {/* <Grid container>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="secondary"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Log In
+                            </Button>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Don't have an account? Sign Up
+                            </Button>
+                            {/* <Grid container>
                             <Grid item xs>
                                 <Link href="#" variant="body2">
                                     Forgot password?
                                 </Link>
                             </Grid>
                             <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"Don't have an account? Sign Up"}
+                            <Link href="#" variant="body2">
+                            {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
                         </Grid> */}
+                        </Box>
+                    </Box>
+                </Container>
             </AnimatedMain>
         </>
     );
