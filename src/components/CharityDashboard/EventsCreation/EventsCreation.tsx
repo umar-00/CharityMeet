623702f89217535c58fc4a0d8ecf7272ec44148d
@MShakeel <mshakeel@genese.de>
2023-03-25 22:24:24 +0100

    related to previous commit

--- src/components/CharityDashboard/EventsCreation/EventsCreation.tsx
+++ src/components/CharityDashboard/EventsCreation/EventsCreation.tsx
@@ -1,15 +1,24 @@
 import { useTheme } from '@mui/system';
 import React from 'react';
 import AnimatedMain from '../../FramerMotion/AnimatedMain';
+import GoogleMap from '../../VolunteerDashboard/Main/GoogleMap/GoogleMap';
 
 type Props = {};
 
-const EventsCreation = (props: Props) => {
+const ViewEventsOnMap = (props: Props) => {
     const theme = useTheme();
 
     return (
+        <AnimatedMain>
+            <div
+                className="h-full w-full"
+                style={{ backgroundColor: theme.palette.background.default }}
+            >
+                <GoogleMap />
+                {/* <h1>EventsCreation</h1> */}
+            </div>
         </AnimatedMain>
     );
 };
 
-export default EventsCreation;
+export default ViewEventsOnMap;
