cdee08528a38aa2c050c59bfb8a3cdd82cd5d999
@MShakeel <mshakeel@genese.de>
2023-04-08 19:46:52 +0200

    FIlter out events in EventsManagement component based logged in user's id

--- src/components/CharityDashboard/EventsManagement/EventsManagement.tsx
+++ src/components/CharityDashboard/EventsManagement/EventsManagement.tsx
@@ -22,14 +22,14 @@ const EventsManagement = (props: Props) => {
     const theme = useTheme();
 
     const user = useStore((state) => state.authenticatedUser);
-    const getEvents = useStore((state) => state.getEventsByCharityId);
+    const getEventsByCharityId = useStore((state) => state.getEventsByCharityId);
     const events = useStore((state) => state.events);
 
     useEffect(() => {
         const getEventsAsync = async (charity_id: string) => {
             console.log('calling getEvents wih charity_id: ', charity_id);
 
-            await getEvents(charity_id);
+            await getEventsByCharityId(charity_id);
         };
 
         if (user) {
@@ -40,41 +40,52 @@ const EventsManagement = (props: Props) => {
     useEffect(() => {
         if (events) {
             console.log('settings events management list item with new events: ', events);
+            const authenticatedUser = useStore.getState().authenticatedUser;
+
+            if (!authenticatedUser) {
+                console.error('User not logged in');
+                return;
+            }
+
             setEventsList(
-                events.map((event) => <EventsManagementListItem key={event.id} event={event} />)
+                events
+                    .filter((e) => e.charity_id === authenticatedUser.user_id)
+                    .map((event) => <EventsManagementListItem key={event.id} event={event} />)
             );
         }
     }, [events]);
 
     return (
-        <AnimatedMain>
-            <div
-                className="h-full w-full overflow-y-auto overflow-x-hidden px-12 py-6"
-                style={{ backgroundColor: theme.palette.background.default }}
-            >
-                <div className="mb-16 flex items-center justify-between">
-                    <h1 className="text-2xl font-bold">Events Management</h1>
+        user && (
+            <AnimatedMain>
+                <div
+                    className="h-full w-full overflow-y-auto overflow-x-hidden px-12 py-6"
+                    style={{ backgroundColor: theme.palette.background.default }}
+                >
+                    <div className="mb-16 flex items-center justify-between">
+                        <h1 className="text-2xl font-bold">Events Management</h1>
 
-                    <Button
-                        size="large"
-                        variant="contained"
-                        color="primary"
-                        endIcon={<AddBoxIcon fontSize="large" />}
-                        onClick={handleClickOpen}
-                    >
-                        <span className="font-semibold">Add event</span>
-                    </Button>
+                        <Button
+                            size="large"
+                            variant="contained"
+                            color="primary"
+                            endIcon={<AddBoxIcon fontSize="large" />}
+                            onClick={handleClickOpen}
+                        >
+                            <span className="font-semibold">Add event</span>
+                        </Button>
 
-                    <AddOrEditEventDialog
-                        handleClose={handleClose}
-                        openDialog={openDialog}
-                        addOrEditString="Add"
-                    />
+                        <AddOrEditEventDialog
+                            handleClose={handleClose}
+                            openDialog={openDialog}
+                            addOrEditString="Add"
+                        />
+                    </div>
+
+                    <div className="flex flex-col items-center gap-y-12">{eventsList}</div>
                 </div>
-
-                <div className="flex flex-col items-center gap-y-12">{eventsList}</div>
-            </div>
-        </AnimatedMain>
+            </AnimatedMain>
+        )
     );
 };
 
